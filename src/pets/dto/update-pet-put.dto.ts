2bb268dbe2451b775ace9b4ebf7f5b16249450ba
Juan Cuervo <cuervojuan0511@gmail.com>
2026-03-03 19:42:45 -0500

    📝 Docs: document pets endpoints

--- src/pets/dto/update-pet-put.dto.ts
+++ src/pets/dto/update-pet-put.dto.ts
@@ -1,24 +1,31 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePetDto } from './create-pet.dto';
 import { IsOptional, IsString, IsUUID, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePetPutDto extends PartialType(CreatePetDto) {
+
+    @ApiProperty({ example: 'Toby' })
     @IsString()
     @MinLength(1)
     name: string;
 
+    @ApiProperty({example: 'Dog'})
     @IsString()
     @MinLength(1)
     species: string;
 
+    @ApiProperty({example: 'Golden Retriever'})
     @IsOptional()
     @IsString()
     breed?: string;
 
+    @ApiProperty({example: '2020-05-10'})
     @IsOptional()
     @IsString()
     birthDate?: string;
 
+    @ApiProperty({example: 'b6d7fffb-4a90-4418-bb93-8d10931fcf73'})
     @IsUUID()
     ownerId: string;
 }
