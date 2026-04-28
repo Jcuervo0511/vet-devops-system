83c74085267ccae8c857cc67c4a684a3f0c07f53
Juan Cuervo <cuervojuan0511@gmail.com>
2026-04-27 19:30:39 -0500

    ✨ Ecr terraform module

---
+++ infra/modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "Prefijo para nombrar los recursos"
+}
+
+variable "repository_name" {
+  type        = string
+  description = "Nombre del repositorio ECR"
+}
